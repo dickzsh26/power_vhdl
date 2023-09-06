f8bfba03237d08f36b7117377e7214625f07bc62
YuQi <qiyu_sjtu@163.com>
2023-09-06 11:10:50 +0800

    新增常数reference

--- user/src/const_reference.v
+++ user/src/const_reference.v
@@ -1,6 +1,10 @@
 module reference(
     input clk,rst_n,
-    output reg[20:0] out);
+    output reg[BIT_WIDTH-1:0] out
+);
+
+parameter BIT_WIDTH = 21;
+parameter [BIT_WIDTH-1:0] OUTPUT_VALUE = 0;
 
 always@(posedge clk or negedge rst_n)
 begin
@@ -10,7 +14,7 @@ begin
     end
     else
     begin
-        out <= 21'd10;
+        out <= OUTPUT_VALUE;
     end
 end
 endmodule
